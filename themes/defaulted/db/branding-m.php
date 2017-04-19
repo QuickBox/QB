d35c2bc9f9732f8a366632d9d3f5ccc60c63c5bf
JMSolo <jmsolodesigns@gmail.com>
2017-04-19 15:58:33 -0500

    add theme switches to dashboard

--- themes/defaulted/db/branding-m.php
+++ themes/defaulted/db/branding-m.php
@@ -1,5 +1,11 @@
 <ul class="dropdown-menu pull-right">
   <li><span style="margin-top:5x"></span></li>
+  <?php $option = array();
+    $option[] = array('file' => 'defaulted', 'title' =>'Defaulted');
+    $option[] = array('file' => 'smoked', 'title' =>'Smoked');
+    foreach($option as $theme) { ?>
+  <li><a href='?themeSelect-<?php echo $theme['file'] ?>=true'><?php echo $theme['title'] ?></a></li>
+  <?php } ?>
   <li><a href="https://plaza.quickbox.io/categories" target="_blank" alt="View all QuickBox Community Categories"><i class="fa fa-fort-awesome" style="color:#e74c3c"></i> Plaza</a></li>
   <li><a href="//quickbox.io/faq" target="_blank" alt="QuickBox FAQs"><i class="fa fa-question" style="color: #25aae2"></i> FAQs</a></li>
   <li><a href="//quickbox.io/wiki" target="_blank" alt="QuickBox Wiki - How-to's, Tips and Tricks"><i class="fa fa-wikipedia-w" style="color:#6c9"></i> Wiki</a></li>
