5e8a323203e0855fdd90c90c20a9e7c6a3fff255
Jason Matthews <jmsolodesigns@gmail.com>
2017-09-10 10:01:13 -0500

    version bump [v2.5.2]

--- dashboard/inc/panel.menu.php
+++ dashboard/inc/panel.menu.php
@@ -30,7 +30,7 @@
                 <ul class="dropdown-menu pull-right" style="font-size: 13px">
                   <li><a href="//quickbox.io/category/announcements/" target="_blank"><?php echo T('ANNOUNCEMENTS'); ?></a></li>
                   <li><a href="https://quickbox.io/readme-md/" target="_blank">README.md</a></li>
-                  <li><a href="https://github.com/QuickBox/QB/compare/v2.5.0...<?php echo $version; ?>" target="_blank">CHANGELOG</a></li>
+                  <li><a href="https://github.com/QuickBox/QB/compare/v2.5.1...<?php echo $version; ?>" target="_blank">CHANGELOG</a></li>
                 </ul>
               </div>
             </li>
